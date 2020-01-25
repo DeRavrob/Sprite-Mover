33374924a96c5da0187e507833bca71ea309e133
Dakota Engelbeck <dakengel@uat.edu>
2020-01-24 21:34:48 -0700

    Added arrow key movements

--- Assets/Scripts/SpriteMover.cs
+++ Assets/Scripts/SpriteMover.cs
@@ -16,16 +16,62 @@ public class SpriteMover : MonoBehaviour
     void Update()
     {
         //Move up if the player presses up.
-        if (Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift))
+        if (Input.GetKey(KeyCode.LeftShift) && Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKey(KeyCode.RightShift) && Input.GetKeyDown(KeyCode.UpArrow))
         {
-            //Only works if shift is held down.
+            //Only works if shift is held down, moves the player up one meter.
+            tf.position = tf.position + Vector3.up * 1.0f;
         }
         else
         {
             //Only works if shift isn't held down.
-            if (Input.GetKey(KeyCode.UpArrow))
+            if (Input.GetKey(KeyCode.UpArrow) &! Input.GetKey(KeyCode.LeftShift))
             {
                 tf.position = tf.position + Vector3.up * Time.deltaTime * speed;
+            }
+        }
+
+        //Move right if the player presses right.
+        if (Input.GetKey(KeyCode.LeftShift) && Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKey(KeyCode.RightShift) && Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            //Only works if shift is held down, moves the player right one meter.
+            tf.position = tf.position + Vector3.right * 1.0f;
+        }
+        else
+        {
+            //Only works if shift isn't held down.
+            if (Input.GetKey(KeyCode.RightArrow) & !Input.GetKey(KeyCode.LeftShift))
+            {
+                tf.position = tf.position + Vector3.right * Time.deltaTime * speed;
+            }
+        }
+
+        //Move left if the player presses left.
+        if (Input.GetKey(KeyCode.LeftShift) && Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKey(KeyCode.RightShift) && Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            //Only works if shift is held down, moves the player left one meter.
+            tf.position = tf.position + Vector3.left * 1.0f;
+        }
+        else
+        {
+            //Only works if shift isn't held down.
+            if (Input.GetKey(KeyCode.LeftArrow) & !Input.GetKey(KeyCode.LeftShift))
+            {
+                tf.position = tf.position + Vector3.left * Time.deltaTime * speed;
+            }
+        }
+
+        //Move down if the player presses down.
+        if (Input.GetKey(KeyCode.LeftShift) && Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKey(KeyCode.RightShift) && Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            //Only works if shift is held down, moves the player down one meter.
+            tf.position = tf.position + Vector3.down * 1.0f;
+        }
+        else
+        {
+            //Only works if shift isn't held down.
+            if (Input.GetKey(KeyCode.DownArrow) & !Input.GetKey(KeyCode.LeftShift))
+            {
+                tf.position = tf.position + Vector3.down * Time.deltaTime * speed;
             }
         }
 
